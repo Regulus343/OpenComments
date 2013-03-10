8a12cce3d5ea79c1f59228dcc14d0ca0ef964118
Regulus343 <codyjassman@gmail.com>
2013-03-10 15:48:34 -0600

    Package created

---
+++ models/Comment.php
@@ -0,0 +1,19 @@
+<?php namespace Regulus\OpenComments;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Comment extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'comments';
+
+	public function addToContent()
+	{
+		return $this->morphTo();
+	}
+
+}
